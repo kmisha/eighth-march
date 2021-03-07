868dd8641a9d7a35b4ab03b439d1a604786b7d37
Mikhail Kolesnikov <dadyaaya@gmail.com>
2021-03-08 01:32:51 +0400

    update implementation
    Signed-off-by: Mikhail Kolesnikov <dadyaaya@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,48 @@
 import html from '@rollup/plugin-html'
 import serve from 'rollup-plugin-serve'
-import {nodeResolve} from '@rollup/plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser'
+import postcss from 'rollup-plugin-postcss'
+import cleanup from 'rollup-plugin-cleanup'
 import {renderFile} from 'pug'
+import postcssPresetEnv from 'postcss-preset-env'
+import autoprefixer from 'autoprefixer'
+
+const isDev = process.env.BUILD === 'dev';
+
+const plugins = [
+    cleanup(),
+    postcss({
+        extract: true,
+        minimize: { preset: 'default' },
+        plugins: [
+            postcssPresetEnv({ stage: 0 }),
+            autoprefixer()
+        ]
+    }),
+    html({
+        title: '8 March',
+        template: ({ attributes, bundle, files, meta, publicPath, title }) => {
+            return renderFile('src/index.pug', {
+                pageTitle: title,
+                style: files.css[0].fileName
+            })
+        }
+    })
+]
+
+if (isDev) {
+    plugins.push(
+        serve({
+            open: false,
+            port: 8080,
+            contentBase: 'dist'
+        })
+    )
+} else {
+    plugins.push(
+        terser()
+    )
+}
 
 export default {
     input: 'src/index.js',
@@ -9,20 +50,8 @@ export default {
         dir: 'dist',
         format: 'es'
     },
-    plugins: [
-        nodeResolve(),
-        html({
-            title: '8 March',
-            template: ({ attributes, bundle, files, meta, publicPath, title }) => {
-                return renderFile('src/index.pug', {
-                    pageTitle: title
-                })
-            }
-        }),
-        serve({
-            open: false,
-            port: 8080,
-            contentBase: 'dist'
-        })
-    ]
+    plugins: plugins,
+    watch: {
+        exclude: 'node_modules/**'
+    }
 }
