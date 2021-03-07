6e56db393a2b32d51e3cd215bdaef5f1532e2a50
Mikhail Kolesnikov <dadyaaya@gmail.com>
2021-03-07 23:20:19 +0400

    created template for HTML
    Signed-off-by: Mikhail Kolesnikov <dadyaaya@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,17 @@
+const html = require('@rollup/plugin-html')
+
 export default {
     input: 'src/index.js',
     output: {
-        file: 'dist/bundle.js',
+        dir: 'dist',
         format: 'es'
-    }
+    },
+    plugins: [
+        html({
+            title: '8 March',
+            meta: [
+                {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+            ]
+        })
+    ]
 }
